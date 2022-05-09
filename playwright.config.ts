756f39ef156de12588f19242393e2b5fcc4a9514
Luis Solier Sajami <luis.sajami@gmail.com>
2022-05-08 20:18:11 -0500

    fix configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import { PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env.CI ? 1 : 0,
+    workers: process.env.CI ? 1 : 1,
     use: {
         trace: 'on-first-retry',
         viewport: { width: 820, height: 1180 },
